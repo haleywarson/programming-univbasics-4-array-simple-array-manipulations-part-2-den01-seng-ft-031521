852d4dd066e70dc51d99fe27a4827ed8080ab82c
Haley Warson <haleywarson@gmail.com>
2021-02-03 20:50:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_concat(array1, array2)
   array.concat(array1, array2)
 end
 
-def using_concat(array1, array2)
-  array.concat(array1, array2)
+def using_insert(array1, array2)
+  array.insert(array1, array2)
 end
 
 def using_concat(array1, array2)
